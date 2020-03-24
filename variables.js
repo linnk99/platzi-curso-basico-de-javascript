be536079c76c004d35db632e99df77d0c9a3b2c1
Hector Vega <linnk99@gmail.com>
2020-03-23 23:03:12 -0500

    Variables class file added

---
+++ variables.js
@@ -0,0 +1,20 @@
+var nombre = "Hector"
+
+// declarar
+var edad
+
+edad
+// inicializar
+edad = 30
+
+edad
+
+var elementos = ["Computadora", "Clular"]
+
+var persona = {
+    nombre: "Isabel",
+    edad: 28
+}
+
+persona
+
