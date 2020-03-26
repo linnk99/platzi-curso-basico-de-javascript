cb549e21bb56f141a8d3ea1880b3213ba7855576
Hector Vega <linnk99@gmail.com>
2020-03-25 22:56:56 -0500

    Logic for choosing options defined

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,12 +1,16 @@
 var options = ["Rock", "Paper", "Scissors"]
 var randomNumber
+var playerChoice
 
-alert("Let's play Rock-Paper=Scissors!!")
-var playerChoice = parseInt(prompt("Choice with a number:\n[1] Rock\n[2] Paper\n[3] Scissors"))
+function playerOption(playerChoice) {
+    alert("Let's play Rock-Paper-Scissors!!")
+    var playerChoice = parseInt(prompt("Choice with a number:\n[1] Rock\n[2] Paper\n[3] Scissors"))
+    return options[playerChoice - 1]
+}
 
 function computerChoice(randomNumber) {
     randomNumber = Math.floor(Math.random() * 3)
     return options[randomNumber]
 }
 
-console.log(`You choosed ${playerChoice} vs ${computerChoice()}`)
+console.log(`You choosed ${playerOption()} vs ${computerChoice()}`)
