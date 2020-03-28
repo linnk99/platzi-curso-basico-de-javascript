7b634cab2604f38f514c3331b1aa7074991a4581
Hector Vega <linnk99@gmail.com>
2020-03-27 23:19:13 -0500

    Trying to define the winning logic after rewriting the script

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,16 +1,20 @@
 var options = ["Rock", "Paper", "Scissors"]
-var randomNumber
-var playerChoice
+// var playerChoice, computerChoice
 
-function playerOption(playerChoice) {
-    alert("Let's play Rock-Paper-Scissors!!")
-    var playerChoice = parseInt(prompt("Choice with a number:\n[1] Rock\n[2] Paper\n[3] Scissors"))
-    return options[playerChoice - 1]
+function player() {
+    alert("Let's play Rock-Paper-Scissors!")
+    var playerChoice = prompt("Choose an option according to a number:\n[1]Rock\n[2]Paper\n[3]Scissors")
+    return playerChoice - 1
 }
 
-function computerChoice(randomNumber) {
-    randomNumber = Math.floor(Math.random() * 3)
-    return options[randomNumber]
+function computer() {
+    var computerChoice = Math.floor(Math.random() * 3)
+    return computerChoice
 }
 
-console.log(`You choosed ${playerOption()} vs ${computerChoice()}`)
+function winningLogic() {
+    player()
+    computer()
+}
+
+winningLogic()
