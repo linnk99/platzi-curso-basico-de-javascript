4df0567a4bdbfe721a4959ab493424cb380047b2
Hector Vega <linnk99@gmail.com>
2020-03-28 01:17:23 -0500

    For and while loop classes added

---
+++ loops.js
@@ -0,0 +1,15 @@
+// for loop
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"]
+
+function saludarEstudiantes(estudiante) {
+    console.log(`Hola, ${estudiante}`)
+}
+
+for (var i = 0; i < estudiantes.length; i++) {
+    saludarEstudiantes(estudiantes[i])
+}
+
+for (var estudiante of estudiantes) {
+    saludarEstudiantes(estudiante)
+}
+
